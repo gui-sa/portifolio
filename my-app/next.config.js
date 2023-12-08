407a10c58e9f6859b79530cfe7416aa620bb2384
gui-sa <guime.sa9@gmail.com>
2023-12-08 13:43:28 -0300

    Teste Git Action

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/github-pages',
+    assetPrefix: '/github-pages',
 }
 
 module.exports = nextConfig
